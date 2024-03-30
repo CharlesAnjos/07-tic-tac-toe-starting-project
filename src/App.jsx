3fac944d060c1b4d69e6304f4e1abe02a7b8d86f
Charles Anjos <charles.ccomp@gmail.com>
2024-03-30 03:38:04 -0300

    86-reducing-state-management

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,47 @@ import { useState } from 'react';
 
 import GameBoard from "./components/GameBoard"
 import Player from "./components/Player"
+import Log from './components/Log';
+
+function deriveCurrentPlayer(gameTurns) {
+  let currentPlayer = 'X';
+
+  if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
+    currentPlayer = 'O';
+  }
+
+  return currentPlayer;
+}
 
 function App() {
-  const [activePlayer, setActivePlayer] = useState('X');
+  const [gameTurns, setGameTurns] = useState([]);
+  const currentPlayer = deriveCurrentPlayer(gameTurns)
   const [isEditable, setIsEditable] = useState(true);
 
-  function handleSelectSquare() {
-    setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
+  function handleSelectSquare(rowIndex, colIndex) {
     setIsEditable(false);
+    setGameTurns((prevTurns) => {
+      const currentPlayer = deriveCurrentPlayer(prevTurns);
+
+      const updatedTurns = [
+        {
+          square: { row: rowIndex, col: colIndex },
+          player: currentPlayer
+        }, ...prevTurns];
+      return updatedTurns;
+    });
   }
 
   return (
     <main>
       <div id="game-container">
         <ol id="players" className="highlight-player">
-          <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'} isEditable={isEditable}/>
-          <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'} isEditable={isEditable}/>
+          <Player initialName="Player 1" symbol="X" isActive={currentPlayer === 'X'} isEditable={isEditable} />
+          <Player initialName="Player 2" symbol="O" isActive={currentPlayer === 'O'} isEditable={isEditable} />
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer}/>
+        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
       </div>
+      <Log turns={gameTurns} />
     </main>
   )
 }
